dd39a99e65376d25673c24e7c11535950f5996b4
luciomcorrea <lucio.24.07@gmail.com>
2023-10-24 23:15:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 let vitorias = 628
-let derrotas = 475
+let derrotas = 540
 let saldoVitorias = vitorias - derrotas
+let nivel
+
 calcNivel()
-
-function calcNivel(vitorias, derrotas) {
-    
-    let nivel    
-
-    if (saldoVitorias <= 10) {nivel = "Ferro"
-    }else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
-        nivel = "Bronze"
-    }else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
-        nivel = "Prata"
-    }else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
-        nivel = "Ouro"
-    }else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
-        nivel = "Diamante"
-    }else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
-        nivel = "Lendário"
-    }else {
-        nivel = "Imortal"
+	function calcNivel (vitorias, derrotas) {
+            if (saldoVitorias <= 10) {
+                nivel = "Ferro"
+    	    }else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+                nivel = "Bronze"
+    	    }else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+                nivel = "Prata"
+    	    }else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+                nivel = "Ouro"
+    	    }else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+                nivel = "Diamante"
+    	    }else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+                nivel = "Lendário"
+    	    }else {
+                nivel = "Imortal"
+    	    }
+        console.log("O Herói tem saldo de " + saldoVitorias + " e está no nível de " + nivel + "!")
     }
-    console.log("O Herói tem de saldo de " + saldoVitorias + " e está no nível de " + nivel + "!")
-}
