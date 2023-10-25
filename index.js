e655379143099278ec6dcf16ac5e1a5176dd1813
luciomcorrea <lucio.24.07@gmail.com>
2023-10-24 21:55:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let pokemon = [
+    ["POOCHYENA", "Male", 2, 13, 13],
+    ["ZIGZAGOON", "Female", 2, 13, 13],
+    ["DRAGONITE", "Male", 5, 25, 25],
+    ["DRAGONITE", "Female", 5, 24, 24],
+    ["DRAGONITE", "Female", 5, 24, 24],
+    ["POOCHYENA", "Female", 3, 15, 15],
+    ["WURMPLE", "Male", 2, 7, 14]
+]//name, gender, level, hp, fullHp
 
+console.log("The pokemon " + pokemon[6][0] + ", of the " + pokemon[6][1] + " gender, Level " + pokemon[6][2] + ", has " + pokemon[6][3] + " HP and " + pokemon[6][4] + " full HP. It has been registered successfully!")
